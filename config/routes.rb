3af6da50bf68be2dc5d02a8572d00924d8cff655
James Riches <jameseldonriches@gmail.com>
2020-05-21 10:25:30 -0800

    editted the routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,15 +1,18 @@
 Rails.application.routes.draw do
-  get 'appointments/index'
-  get 'appointments/new'
-  get 'appointments/show'
-  get 'appointments/edit'
-  get 'doctors/index'
-  get 'doctors/new'
-  get 'doctors/show'
-  get 'doctors/edit'
-  get 'users/index'
-  get 'users/new'
-  get 'users/show'
-  get 'users/edit'
+  # get 'appointments/index'
+  # get 'appointments/new'
+  # get 'appointments/show'
+  # get 'appointments/edit'
+  # get 'doctors/index'
+  # get 'doctors/new'
+  # get 'doctors/show'
+  # get 'doctors/edit'
+  # get 'users/index'
+  # get 'users/new'
+  # get 'users/show'
+  # get 'users/edit'
+  resources :users
+  resources :doctors
+  resources :appointments
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
