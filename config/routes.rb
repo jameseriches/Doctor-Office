3b411c5223afd4f2ed19abe027222f1d719dc4f3
James Riches <jameseldonriches@gmail.com>
2020-05-21 10:05:33 -0800

    added controllers and models

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,15 @@
 Rails.application.routes.draw do
+  get 'appointments/index'
+  get 'appointments/new'
+  get 'appointments/show'
+  get 'appointments/edit'
+  get 'doctors/index'
+  get 'doctors/new'
+  get 'doctors/show'
+  get 'doctors/edit'
+  get 'users/index'
+  get 'users/new'
+  get 'users/show'
+  get 'users/edit'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
